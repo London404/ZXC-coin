78c4e2c735fdf6e6f4c99051ff3f7a346d24cc94
London404 <112338355+London404@users.noreply.github.com>
2024-07-16 00:42:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-jQuery("body").append("<a href='https://www.google.com.ua/'>chat gpt</a>");
-var click;
-click = 0;
-click = parseInt(click);
-jQuery("button").on("click", function () {   
-    var all_click;
-    all_click = click + 1;
-    jQuery("body").append(all_click);
 
-});
