b8d4ba262d37a6b37da05a91656076e36cdea1cb
London404 <112338355+London404@users.noreply.github.com>
2024-08-19 21:51:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
 let wave = document.getElementById("wave");
 let curr_track = document.createElement("audio");
-wave.style.display = "none";
+
 let track_index = 0;
 let isPlaying = false;
 let isRandom = true;
@@ -284,3 +284,34 @@ document.getElementById('form-ui').addEventListener('submit', function(e) {
       alert('Будь ласка введіть ім`я та пароль!');
     }
   });
+  function handleClick(buttonId) {
+    localStorage.setItem('selectedButton', buttonId);
+
+    if (buttonId === 'choose1') {
+        window.location.href = "index.html";
+    } else if (buttonId === 'choose2') {
+        window.location.href = "test.html";
+    } else if (buttonId === 'choose3') {
+        window.location.href = "up.html";
+    } else if (buttonId === 'choose4') {
+        window.location.href = "set.html";
+    } else if (buttonId === 'choose5') {
+        window.location.href = "lvl.html";
+    }
+}
+
+function loadSelectedButton() {
+    const selectedButton = localStorage.getItem('selectedButton');
+    if (selectedButton) {
+        document.getElementById(selectedButton).checked = true;
+    }
+}
+if (window.location.pathname.includes("index.html")) {
+    console.log("Находимся на главной странице");
+} else if (window.location.pathname.includes("test.html")) {
+    console.log("Находимся на тестовой странице");
+}
+
+window.onload = loadSelectedButton;
+
+wave.style.display = "none";
